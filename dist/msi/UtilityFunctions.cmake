830d57e2d0e3e09bfd930f2ce12e5e1404fdaa16
Charles Kerr <charles@charleskerr.com>
2022-07-03 09:30:30 -0500

    fixup! feat: use jimmac ratio icon in qt client
    fix: windows build relied on qt/icons/ png

--- dist/msi/UtilityFunctions.cmake
+++ dist/msi/UtilityFunctions.cmake
@@ -39,7 +39,7 @@ function(find_msvc_crt_msm OUTPUT_VAR)
     set(${OUTPUT_VAR} "${${OUTPUT_VAR}}" PARENT_SCOPE)
 endfunction()
 
-function(png2ico OUTPUT_FILE)
+function(svg2ico OUTPUT_FILE)
     if(NOT IS_ABSOLUTE "${OUTPUT_FILE}")
         set(OUTPUT_FILE "${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_FILE}")
     endif()
